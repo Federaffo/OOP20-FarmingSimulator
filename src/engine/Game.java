6d5ebad5abb31c325664c7f245676891744bacee
Federico Raffoni <36224988+Federaffo@users.noreply.github.com>
2021-03-01 14:33:11 +0100

    added class Game

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,5 +1,23 @@
 package engine;
 
+import java.awt.event.KeyEvent;
+
 public class Game {
+
+    public void loop() {
+        
+    }
+    
+    public void update() {
+        
+    }
+
+    public void keyPressed(KeyEvent e) {
+       
+    }
+
+    public void keyReleased(KeyEvent e) {
+       
+    }
     
 }
