95806a844e37ed3329407d2e1b65cb7ad8e23fae
unknown <giacomo.montali@studio.unibo.it>
2021-03-02 14:38:27 +0100

    added class Player and Direction

---
+++ src/entity/Direction.java
@@ -0,0 +1,38 @@
+package entity;
+
+public class Direction {
+	
+	private boolean up;
+	private boolean down;
+	private boolean left;
+	private boolean right;
+	
+	public Direction() {
+		
+	}
+	
+	public boolean isUp() {
+		return up;
+	}
+	public void setUp(boolean up) {
+		this.up = up;
+	}
+	public boolean isDown() {
+		return down;
+	}
+	public void setDown(boolean down) {
+		this.down = down;
+	}
+	public boolean isLeft() {
+		return left;
+	}
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+	public boolean isRight() {
+		return right;
+	}
+	public void setRight(boolean right) {
+		this.right = right;
+	}
+}
---
+++ src/entity/Player.java
@@ -0,0 +1,22 @@
+package entity;
+
+public class Player {
+	
+	private String name;
+	private Pair<Integer, Integer> pos;
+	private Object bag;
+	
+	public Player(Pair<Integer, Integer> position) {
+		pos = position;
+	}
+	public Player(Object inventory) {
+		bag = inventory;
+	}
+	
+	public void move(Direction d){
+		if(d.isUp()) pos.setY(pos.getY()+1);
+		if(d.isDown()) pos.setY(pos.getY()-1);
+		if(d.isLeft()) pos.setX(pos.getX()-1);
+		if(d.isRight()) pos.setX(pos.getX()+1);
+	}
+}
