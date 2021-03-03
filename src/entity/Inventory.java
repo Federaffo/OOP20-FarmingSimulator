2cd2a69583f5c95f439355465ec9d40c832a607f
Federico Raffoni <36224988+Federaffo@users.noreply.github.com>
2021-03-03 22:08:32 +0100

    fixed bug getCurrentSeed in Inventory with Montali

--- src/entity/Inventory.java
+++ src/entity/Inventory.java
@@ -2,6 +2,7 @@ package entity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Queue;
@@ -40,7 +41,6 @@ public class Inventory {
 		moveBottom(activeSeed.element());
 		
 		for (SeedType seedType : activeSeed) {
-			
 			if(seeds.get(seedType) > 0) {
 				moveTop(seedType);
 				isThereASeed = true;
@@ -51,25 +51,28 @@ public class Inventory {
 		if(isThereASeed == false) {
 			return Optional.empty();
 		}else {
-			return Optional.of(new Pair<SeedType, Integer>(activeSeed.element(), seeds.get(activeSeed)));
+			return Optional.of(new Pair<SeedType, Integer>(activeSeed.element(), seeds.get(activeSeed.element())));
 		}
 	}
+	
 	//return the current seed selected
 	public Optional<Pair<SeedType, Integer>> getCurrentSeed(){
-		if(seeds.get(activeSeed) > 0) {
-			return Optional.of(new Pair<SeedType, Integer>(activeSeed.element(), seeds.get(activeSeed)));
+		if(seeds.get(activeSeed.element()) > 0) {
+			return Optional.of(new Pair<SeedType, Integer>(activeSeed.element(), seeds.get(activeSeed.element())));
 		}else {
 			return Optional.empty();
 		}
 	}
+	
 	//move the object to the bottom
 	private void moveBottom(SeedType type) {
 		activeSeed.remove(type);
 		activeSeed.add(type);
 	}
+	
 	//move the object to the top
 	private void moveTop(SeedType type) {
-		for (SeedType seedType : activeSeed) {
+		for (SeedType seedType : List.copyOf(activeSeed)) {
 			if(!seedType.equals(type)) {
 				moveBottom(seedType);
 			}
@@ -79,14 +82,13 @@ public class Inventory {
 	//add {number} seeds of {type} type to inventory
 	public void addSeeds(SeedType type, Integer number) {
 		seeds.put(type, seeds.get(type) + number);
-		
-		//set the activeSeed to the seed added to inventory
-		moveTop(type);
+		moveTop(type); //set the activeSeed to the seed added to inventory	
 	}
+	
+	
 	//remove 1 seed of {type} type
 	public void removeSeed(SeedType type) {
 		seeds.put(type, seeds.get(type) - 1);
-		
 		if(seeds.get(type) == 0) {
 			
 		}
