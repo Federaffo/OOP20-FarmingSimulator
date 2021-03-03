4dd34fca3d877c507c617650b44f7cfd84079d5a
Federico Raffoni <36224988+Federaffo@users.noreply.github.com>
2021-03-03 12:35:01 +0100

    Added getPlayer to Game

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,66 +1,57 @@
 package engine;
 
 import java.awt.event.KeyEvent;
+import java.util.Map;
+
+import entity.Inventory;
+import entity.Pair;
+import entity.Player;
 
 public class Game {
-
-	public int x = 0;
-	public int y = 0;
-	
-	private boolean w= false;
-	private boolean a= false;
-	private boolean s= false;
-	private boolean d= false;
-
+	private Player pg = new Player(new Pair<>(1, 1));
+	private Map map;
 
 	public void loop() {
-		if(w) {
-			y-=5;
-		}
-		if(a) {
-			x-=5;
-		}
-		if(s) {
-			y+=5;
-		}
-		if(d) {
-			x+=5;
-		}
+		pg.move();
 	}
 
-	public void update() {
+	public Map getMap() {
+		return this.map;
+	}
 
+	public Player getPlayer() {
+		return this.pg;
 	}
 
 	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode() == 87) {
-			w=true;
+		if (e.getKeyCode() == KeyEvent.VK_W) {
+			pg.setUp(true);
 		}
-		if(e.getKeyCode() == 65) {
-			a=true;
+		if (e.getKeyCode() == KeyEvent.VK_A) {
+			pg.setLeft(true);
 		}
-		if(e.getKeyCode() == 83) {
-			s=true;
+		if (e.getKeyCode() == KeyEvent.VK_S) {
+			pg.setDown(true);
 		}
-		if(e.getKeyCode() == 68) {
-			d=true;
+		if (e.getKeyCode() == KeyEvent.VK_D) {
+			pg.setRight(true);
 		}
-
 	}
 
 	public void keyReleased(KeyEvent e) {
-		if(e.getKeyCode() == 87) {
-			w=false;
+		if (e.getKeyCode() == KeyEvent.VK_W) {
+			pg.setUp(false);
 		}
-		if(e.getKeyCode() == 65) {
-			a=false;
+		if (e.getKeyCode() == KeyEvent.VK_A) {
+			pg.setLeft(false);
 		}
-		if(e.getKeyCode() == 83) {
-			s=false;
+		if (e.getKeyCode() == KeyEvent.VK_S) {
+			pg.setDown(false);
 		}
-		if(e.getKeyCode() == 68) {
-			d=false;
+		if (e.getKeyCode() == KeyEvent.VK_D) {
+			pg.setRight(false);
 		}
+
 	}
 
 }
