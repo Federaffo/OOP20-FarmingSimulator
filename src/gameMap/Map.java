75d747df88f6c4d2070de9532233b873bd068634
unknown <giacomo.montali@studio.unibo.it>
2021-03-03 16:38:43 +0100

    update Map generator

--- src/gameMap/Map.java
+++ src/gameMap/Map.java
@@ -1,18 +1,22 @@
 package gameMap;
 
+import java.util.Random;
 
 public class Map {
 	
 	private Block[][] mappa;
 	
 	public Map(int x, int y) {
-
+	
 		mappa = new Block[x][y];
 		FactoryBlock factory = new FactoryBlock();
 		
 		for(int i=0; i<x; i++) {
 			for(int j=0; j<y; j++) {
-				mappa[i][j] = factory.getTerrainBlock();
+				if(new Random().nextInt(100) < 20)
+					mappa[i][j] = factory.getTerrainBlock();
+				else
+					mappa[i][j] = factory.getFieldBlock();
 			}
 		}
 	}
