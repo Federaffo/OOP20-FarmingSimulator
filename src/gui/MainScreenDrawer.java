308136845b022a85240677ed2461af672e1af5f8
Federico Raffoni <36224988+Federaffo@users.noreply.github.com>
2021-03-03 09:13:17 +0100

    added simple movment graphics system

--- src/gui/MainScreenDrawer.java
+++ src/gui/MainScreenDrawer.java
@@ -1,27 +1,52 @@
 package gui;
 
-import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.io.File;
+import java.io.IOException;
 
-import javax.swing.JPanel;
-
+import javax.imageio.ImageIO;
 import engine.Game;
 
-public class MainScreenDrawer extends GameDrawer{
-    private static final long serialVersionUID = -8051528011999726915L;
+public class MainScreenDrawer extends GameDrawer {
+	private static final long serialVersionUID = -8051528011999726915L;
+	private int indx = 0;
+	private String right = "res/right";
+	private String left = "res/left";
+	private String png = ".png";
 
-    public MainScreenDrawer(Game game) {
-        super(game);
-        // TODO Auto-generated constructor stub
-    }
+	public MainScreenDrawer(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
 
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        var g2d = (Graphics2D) g;
-        g2d.setColor(Color.red);
-        g2d.drawRect(game.x, game.y, 50, 50);
-        //super.repaint();
-    }
+	@Override
+	public void paintComponent(Graphics g) {
+
+
+		super.paintComponent(g);
+		drawPg(g);
+
+		// super.repaint();
+	}
+
+	private void drawPg(Graphics g) {
+		indx += 1;
+		indx %= 20;
+		int x = indx / 10;
+		try {
+			if (game.getPlayer().isMoving()) {
+				
+				if (game.getPlayer().isFacingRight()) {
+					g.drawImage(ImageIO.read(new File(right + x + png)), game.getPlayer().getX(),game.getPlayer().getY(), 40, 70, null);
+				} else {
+					g.drawImage(ImageIO.read(new File(left + x + png)), game.getPlayer().getX(),
+							game.getPlayer().getY(), 40, 70, null);
+				}
+			}else {
+				g.drawImage(ImageIO.read(new File("res/stop.png")), game.getPlayer().getX(),game.getPlayer().getY(), 40, 70, null);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
