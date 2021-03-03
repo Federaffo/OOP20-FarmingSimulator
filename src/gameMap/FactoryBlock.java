e088256718ee10f400bb553ee840127f424cd1e2
Linda Fabbri <linda.fabbri5@studio.unibo.it>
2021-03-03 12:58:32 +0100

    Implemented FieldBlock

--- src/gameMap/FactoryBlock.java
+++ src/gameMap/FactoryBlock.java
@@ -1,8 +1,13 @@
 package gameMap;
 
+import java.awt.Rectangle;
 import java.util.*;
 
+import entity.Pair;
+import item.Food;
+import item.FoodType;
 import item.Seed;
+import item.SeedType;
 
 
 
@@ -21,14 +26,18 @@ public class FactoryBlock {
 	public Block getFieldBlock(){
 		return new Block(BlockType.FIELD, true, true){
 			private Optional<Seed> seed = Optional.empty();
-			public void interact() {
-				
+			public void plant() {
+				seed = Optional.of(new Seed(SeedType.POTATO_SEED));
+			}
+			public Pair<Food, Integer> harvest() {
+				Food food = seed.get().Harvest();
+				return new Pair<>(food,3);
 			}
 		};
 	}
 	
 
-	private class Block {
+	private class Block extends Rectangle{
 		
 		public final static int SIZE = 50;
 		private final BlockType blockType;
