4e9c5f07baa5f6129d069f41f5bdf4ad071bc3f3
Linda <linda.fabbri5@studio.unibo.it>
2021-03-04 16:19:37 +0100

    Added position to block

--- src/gameMap/FactoryBlock.java
+++ src/gameMap/FactoryBlock.java
@@ -5,64 +5,64 @@ import java.util.*;
 
 import entity.Pair;
 import item.Food;
-import item.FoodType;
 import item.Seed;
 import item.SeedType;
 
-
-
-
-
 public class FactoryBlock {
-	
-	public BlockImpl getTerrainBlock(){
-		return new BlockImpl(BlockType.TERRAIN, true, false);
+
+	public BlockImpl getTerrainBlock(int posx, int posy) {
+		return new BlockImpl(BlockType.TERRAIN, true, false, posx, posy);
 	}
-	
-	public BlockImpl getObstacleBlock(){
-		return new BlockImpl(BlockType.OBSTACLE, false, false);
+
+	public BlockImpl getObstacleBlock(int posx, int posy) {
+		return new BlockImpl(BlockType.OBSTACLE, false, false, posx, posy);
 	}
-	
-	public BlockImpl getFieldBlock(){
-		return new BlockImpl (BlockType.FIELD, true, true){
+
+	public BlockImpl getFieldBlock(int posx, int posy) {
+		return new BlockImpl(BlockType.FIELD, true, true, posx, posy) {
 			private Optional<Seed> seed = Optional.empty();
+
 			public void plant() {
 				seed = Optional.of(new Seed(SeedType.POTATO_SEED));
 			}
+
 			public Pair<Food, Integer> harvest() {
 				Food food = seed.get().Harvest();
-				return new Pair<>(food,3);
+				return new Pair<>(food, 3);
 			}
 		};
 	}
-	
 
-	private class BlockImpl extends Rectangle implements Block{
-		
+	private class BlockImpl extends Rectangle implements Block {
+
 		public final static int SIZE = 50;
 		private final BlockType blockType;
 		private final boolean isWalkable;
 		private final boolean isInteractable;
+		private final int posx;
+		private final int posy;
 
-		public BlockImpl(BlockType bt, boolean isWalkable, boolean isInteractable) {
+		public BlockImpl(BlockType bt, boolean isWalkable, boolean isInteractable, int posx, int posy) {
+			super();
 			this.blockType = bt;
 			this.isWalkable = isWalkable;
 			this.isInteractable = isInteractable;
+			this.posx = posx;
+			this.posy = posy;
 		}
-		
 
 		public boolean isWalkable() {
-			return this.isWalkable();
+			return this.isWalkable;
 		}
-		
+
 		public boolean isInteractable() {
-			return this.isInteractable();
+			return this.isInteractable;
 		}
-		
+
 		public BlockType getType() {
 			return this.blockType;
 		}
 
 	}
-	
+
 }
