698887b17b6a17db07f776bde323033259bd2d51
Linda <linda.fabbri5@studio.unibo.it>
2021-04-10 15:09:42 +0200

    Sistemate alcune cose nello ShopDrawer

--- src/gui/ShopDrawer.java
+++ src/gui/ShopDrawer.java
@@ -3,9 +3,11 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.ComponentOrientation;
 import java.awt.Dialog;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
@@ -26,11 +28,14 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.ListModel;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.SpringLayout;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.StyleConstants;
 
 import static javax.swing.JOptionPane.showMessageDialog;
 
@@ -62,12 +67,22 @@ public class ShopDrawer extends GameDrawer {
 		JPanel titlePanel = new JPanel();
 		titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.Y_AXIS));
 		JTextArea title = new JTextArea("WELCOME TO THE SHOP!");
+		
+		title.setForeground(Color.WHITE);
+		Font font = new Font("Garamond", Font.BOLD, 50);
+		title.setFont(font);
+		title.setLineWrap(true);
 		JTextArea descr = new JTextArea("Qui puoi compare materiali e vendere i tuoi oggetti");
+		descr.setForeground(Color.WHITE);
+		Font font2 = new Font("Segoe Script", Font.BOLD, 20);
+		descr.setFont(font2);
+		descr.setLineWrap(true);
 		title.setEditable(false);
 		descr.setEditable(false);
+		
 		title.setBorder(BorderFactory.createEmptyBorder(TOPB, LEFTB, 0, RIGHTB));
 		descr.setBorder(BorderFactory.createEmptyBorder(0, LEFTB, BOTTOMB, RIGHTB));
-		titlePanel.setBackground(new Color(255, 0, 0, 200));
+		titlePanel.setBackground(new Color(17, 96, 98));
 		title.setBackground(titlePanel.getBackground());
 		descr.setBackground(titlePanel.getBackground());
 		titlePanel.add(title);
@@ -77,15 +92,15 @@ public class ShopDrawer extends GameDrawer {
 		/* Pannello buy */
 
 		JPanel buyPanel = new JPanel();
-
+		buyPanel.add(Box.createRigidArea(new Dimension(0,80)));
 		String[] itemString = new String[countSeed];
 		int i = 0;
 		for (SeedType seed : g.getShop().getSeedItemList()) {
 			itemString[i++] = seed.getName();
 		}
 
-		buyPanel.setBackground(Color.PINK);
-		buyPanel.setLayout(new BoxLayout(buyPanel, BoxLayout.Y_AXIS));
+		buyPanel.setBackground(new Color(17, 96, 98));
+		buyPanel.setLayout(new BoxLayout(buyPanel, BoxLayout.PAGE_AXIS));
 		buyPanel.setBorder(BorderFactory.createEmptyBorder(TOPB, LEFTB, BOTTOMB, RIGHTB));
 
 		JComboBox<Object> selectSeed = new JComboBox<>(itemString);
@@ -94,20 +109,27 @@ public class ShopDrawer extends GameDrawer {
 		int startValue = 0, minValue = 0, maxValue = 1000, step = 1;
 		JSpinner quantity = new JSpinner(new SpinnerNumberModel(startValue, minValue, maxValue, step));
 		buyPanel.add(quantity);
-
-		JLabel prezzoTot = new JLabel("" + (SeedType.getSeedType(selectSeed.getSelectedItem().toString()).getPrice())
+		
+		
+		JTextField prezzoTot = new JTextField("TOT: " + (SeedType.getSeedType(selectSeed.getSelectedItem().toString()).getPrice())
 				* ((Integer) quantity.getValue()));
+		prezzoTot.setEditable(false);
+		prezzoTot.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		//prezzoTot.setText("" + (SeedType.getSeedType(selectSeed.getSelectedItem().toString()).getPrice())* ((Integer) quantity.getValue()));
+		
+		//prezzoTot.setBorder(BorderFactory.createEmptyBorder(0, 450, 0, 0));
+		//buyPanel.add(Box.createRigidArea(new Dimension(0,80)));
 		buyPanel.add(prezzoTot);
-
+		buyPanel.add(Box.createRigidArea(new Dimension(0,80)));
 		JButton buy = new JButton("COMPRAAAAA");
-		buyPanel.add(buy);
-
+		buyPanel.add(buy, -1);
+		
 		quantity.addChangeListener(new ChangeListener() {
 
 			@Override
 			public void stateChanged(ChangeEvent e) {
 				// TODO Auto-generated method stub
-				prezzoTot.setText(
+				prezzoTot.setText("TOT: " +
 						Double.toString((SeedType.getSeedType(selectSeed.getSelectedItem().toString()).getPrice())
 								* ((Integer) quantity.getValue())));
 			}
@@ -117,7 +139,7 @@ public class ShopDrawer extends GameDrawer {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				prezzoTot.setText(
+				prezzoTot.setText("TOT: " +
 						Double.toString((SeedType.getSeedType(selectSeed.getSelectedItem().toString()).getPrice())
 								* ((Integer) quantity.getValue())));
 			}
@@ -143,7 +165,7 @@ public class ShopDrawer extends GameDrawer {
 		JPanel sellPanel = new JPanel();
 		sellPanel.setLayout(new BoxLayout(sellPanel, BoxLayout.Y_AXIS));
 		sellPanel.setBorder(BorderFactory.createEmptyBorder(TOPB, LEFTB, BOTTOMB, RIGHTB));
-		sellPanel.setBackground(Color.CYAN);
+		sellPanel.setBackground(new Color(17, 96, 98));
 		JButton sellAll = new JButton("SELL ALL");
 		sellPanel.add(sellAll);
 
@@ -164,7 +186,7 @@ public class ShopDrawer extends GameDrawer {
 		inventPanel.add(new JLabel(""));
 		inventPanel.add(new JLabel("Inventario"));
 		inventPanel.add(new JLabel(""));
-		inventPanel.setBackground(Color.ORANGE);
+		inventPanel.setBackground(new Color(17, 96, 98));
 
 		/* Fine Pannello Inventario */
 
