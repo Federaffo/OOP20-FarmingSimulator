45710673c7a27b66a96f1662af50d29c98f72fb5
SimoRega <simone.rega2@studio.unibo.it>
2021-04-18 22:46:24 +0200

    Test Player Movement done

--- src/Test/FarmingSimulatorTestClass.java
+++ src/Test/FarmingSimulatorTestClass.java
@@ -2,16 +2,13 @@ package Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import java.util.stream.IntStream;
+
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import engine.Engine;
 import engine.Game;
-import engine.GameImpl;
 import entity.Pair;
 import entity.Player;
 import gameMap.BlockType;
@@ -20,38 +17,45 @@ import gameMap.Map;
 
 public class FarmingSimulatorTestClass {
 	private final int BLOCKSIZE=50;
-	private GameImpl g = null;
+	private Game g = null;
 	private Player pg = null;
 	private Map map= null;
 	
 	@BeforeEach
 	public void initGame() {
 		Engine engine = new Engine();
-        engine.CreateGame();
-		Game g=engine.getGame();
+        engine.update(false);
+		g=engine.getGame();
 		pg=g.getPlayer();
-		map=g.getMap();
-		System.out.println(g+" init");
-		
+		map=g.getMap();	
 	}
 	
 	@Test
 	public void testPlayerMovement() {
 		pg.setUp(true);
+		IntStream.range(0,10).forEach(i->g.loop());
 		assertEquals(new Pair<>(50,50),new Pair<>(pg.getPosX(),pg.getPosY()));
-		pg.setLeft(true);
-		assertEquals(new Pair<>(50,50),new Pair<>(pg.getPosX(),pg.getPosY()));
-		pg.setDown(true);
-		assertEquals(new Pair<>(50,100),new Pair<>(pg.getPosX(),pg.getPosY()+BLOCKSIZE));
-		pg.setRight(true);
-		assertEquals(new Pair<>(100,100),new Pair<>(pg.getPosX()+BLOCKSIZE,pg.getPosY()+BLOCKSIZE));
+		pg.setUp(false);
 		
+		pg.setLeft(true);
+		IntStream.range(0,10).forEach(i->g.loop());
+		assertEquals(new Pair<>(50,50),new Pair<>(pg.getPosX(),pg.getPosY()));
+		pg.setLeft(false);
+		
+		pg.setDown(true);
+		IntStream.range(0,10).forEach(i->g.loop());
+		assertEquals(new Pair<>(50,100),new Pair<>(pg.getPosX(),pg.getPosY()));
+		pg.setDown(false);
+		
+		pg.setRight(true);
+		IntStream.range(0,10).forEach(i->g.loop());
+		assertEquals(new Pair<>(100,100),new Pair<>(pg.getPosX(),pg.getPosY()));
+		pg.setRight(false);
 		
 	}
 
 	@Test
-	public void testPlayerPlant() {
-		System.out.println(g+" plant");
+	public void testPlayerPlant() {		
 		assertEquals(map.getBlock(2, 2).getType(),BlockType.FIELD);
 		assertEquals(((FieldBlock)map.getBlock(2,2)).isEmpty(),true);
 		g.interact();
