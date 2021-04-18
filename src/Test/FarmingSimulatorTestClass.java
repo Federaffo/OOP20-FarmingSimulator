5dc0cd0b85f3e09fb8877acdb9a3b584f3b6a8ef
SimoRega <simone.rega2@studio.unibo.it>
2021-04-18 23:17:25 +0200

    Added Plant Test

--- src/Test/FarmingSimulatorTestClass.java
+++ src/Test/FarmingSimulatorTestClass.java
@@ -1,6 +1,8 @@
 package Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.stream.IntStream;
 
@@ -32,21 +34,25 @@ public class FarmingSimulatorTestClass {
 	
 	@Test
 	public void testPlayerMovement() {
+		//Muovo il personaggio verso l'alto e controllo la sua posizione
 		pg.setUp(true);
 		IntStream.range(0,10).forEach(i->g.loop());
 		assertEquals(new Pair<>(50,50),new Pair<>(pg.getPosX(),pg.getPosY()));
 		pg.setUp(false);
 		
+		//Muovo il personaggio verso sinistra e controllo la sua posizione
 		pg.setLeft(true);
 		IntStream.range(0,10).forEach(i->g.loop());
 		assertEquals(new Pair<>(50,50),new Pair<>(pg.getPosX(),pg.getPosY()));
 		pg.setLeft(false);
 		
+		//Muovo il personaggio verso il basso e controllo la sua posizione
 		pg.setDown(true);
 		IntStream.range(0,10).forEach(i->g.loop());
 		assertEquals(new Pair<>(50,100),new Pair<>(pg.getPosX(),pg.getPosY()));
 		pg.setDown(false);
 		
+		//Muovo il personaggio verso destra e controllo la sua posizione
 		pg.setRight(true);
 		IntStream.range(0,10).forEach(i->g.loop());
 		assertEquals(new Pair<>(100,100),new Pair<>(pg.getPosX(),pg.getPosY()));
@@ -55,11 +61,21 @@ public class FarmingSimulatorTestClass {
 	}
 
 	@Test
-	public void testPlayerPlant() {		
+	public void testPlayerPlant() {	
+		//muovo il personaggio in diagonale verso il blocco (2,2)
+		pg.setDown(true);
+		pg.setRight(true);
+		IntStream.range(0,10).forEach(i->g.loop());
+		//Controllo che il player sia sul blocco (2,2)
+		assertEquals(map.getBlock(2, 2),pg.blockPosition(map.getMapSet()));
+		//Controllo che il blocco (2,2) sia di tipo FIELD
 		assertEquals(map.getBlock(2, 2).getType(),BlockType.FIELD);
-		assertEquals(((FieldBlock)map.getBlock(2,2)).isEmpty(),true);
+		//Controllo che il blocco sia vuoto, cioè non ha nessun seme piantato
+		assertTrue(((FieldBlock)map.getBlock(2,2)).isEmpty());
+		//Pianto un seme nel blocco (2,2)
 		g.interact();
-		assertEquals(((FieldBlock)map.getBlock(2,2)).isEmpty(),false);
+		//Controllo che il blocco abbia il seme dopo averci interagito e aver inserito un seme
+		assertFalse(((FieldBlock)map.getBlock(2,2)).isEmpty());
 	}
 
 	public void testPlayerAnimalInteraction() {
