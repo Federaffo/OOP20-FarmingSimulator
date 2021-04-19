6531b18d73119c6b5825444587903fa53b8d1a3e
SimoRega <simone.rega2@studio.unibo.it>
2021-04-19 11:20:44 +0200

    Fixed InfoDrawer and ShopDrawer

--- src/gui/InfoDrawer.java
+++ src/gui/InfoDrawer.java
@@ -20,8 +20,12 @@ public class InfoDrawer extends GameDrawer {
 	}
 
 	private void generateInfoPanel(Game g, Dimension screenSize) {
-		final int height = (int) (screenSize.height * 0.1);
-		final int width = (int) (screenSize.width * 0.5);
+		final int height = (int) (screenSize.height * 0.09);
+		final int width = (int) (screenSize.width * 0.6);
+		
+		final int titleHeight = height/2;
+		final int titleWidth = width/2;
+		
 		final int RGTB = (int) (screenSize.width * 0.22);
 		final int LFTB = (int) (screenSize.width * 0.22);
 		final int UPPB = (int) (screenSize.width * 0.03);
@@ -34,13 +38,13 @@ public class InfoDrawer extends GameDrawer {
 		infoPanel.setBackground(new Color(255, 255, 204, 100));
 		infoPanel.setBorder(BorderFactory.createEmptyBorder(UPPB, LFTB, BTMB, RGTB));
 
-		infoPanel.add(new JPanelINFO("General Commands", height / 2, width / 2));
+		infoPanel.add(new JPanelINFO("General Commands", titleHeight, titleWidth));
 		infoPanel.add(new JPanelINFO(height, width, texture.WASD, "MOVEMENT ", "[W-A-S-D] type this button to move"));
 		infoPanel.add(new JPanelINFO(height, width, texture.F_KEY, "INTERACT", "[F] press this to plant and harvest"));
 		infoPanel.add(new JPanelINFO(height, width, texture.E_KEY, "MARKET", "[E] press to enter the shop menu view"));
 		infoPanel.add(new JPanelINFO(height, width, texture.MOUSE_WHEEL, "HOTBAR", "[MOUSE_WHEEL] change hotbar selection"));
 
-		infoPanel.add(new JPanelINFO("Game HUD Info", height / 2, width / 2));
+		infoPanel.add(new JPanelINFO("Game HUD Info", titleHeight, titleWidth));
 		infoPanel.add(new JPanelINFO(height, width, texture.MONEY, "MONEY",
 				"show the current amount of money the player have"));
 		infoPanel.add(new JPanelINFO(height, width, texture.LOCK, "LOCK",
